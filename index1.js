c90120b20ed34ff8cdac4e9ac8466de4348b3a13
Durga <durgamohapatra303@gmail.com>
2022-10-11 12:07:58 +0530

    Jscript Interview Question & Answer

---
+++ index1.js
@@ -0,0 +1,157 @@
+//1
+
+// function fruit(){
+//     console.log(name)
+//     console.log(price)
+//     var name="Apple"
+//     let price=20
+// }
+// fruit()
+
+
+//2
+
+// for(var i=0;i<3;i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 2000);
+// }
+
+
+//3
+
+// for(let i=0;i<3;i++){
+//         setTimeout(() => {
+//             console.log(i)
+//         }, 2000);
+//     }
+
+
+//4
+
+// console.warn(+true)
+// console.warn(typeof +true)
+
+
+//5
+
+// console.warn(!"anil")
+// console.warn(typeof("anil"))
+
+
+//6
+/*
+let c={
+    name:"Peter"
+}
+let d
+
+d=c
+// c.name="anil"
+console.log(d.name)
+*/
+
+//7
+
+// var x
+// var x=10
+// console.log(x)
+
+
+//8
+
+// var x
+// let x=10
+// console.log(x)
+
+//9
+
+// let a=3
+// let b=new Number(3)
+
+// console.log(a==b)
+// console.log(a===b)
+
+//10
+
+/*
+function fruit(){
+    console.log("woof!")
+}
+// fruit.name="apple"
+// console.log(fruit())
+fruit()
+*/
+
+//11
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(1,"2"))
+
+//12
+
+// let number=0
+
+// console.log(number++)
+// console.log(++number)
+// console.log(number)
+
+//13
+
+// function getAge(...arg){
+//     console.log(arg)
+//     console.log(typeof arg)
+// }
+// getAge(21)
+
+//14
+
+// function getAge(){
+//     'use strict'
+//      age=21
+//     console.log(age)
+// }
+// getAge()
+
+
+//15
+
+// const sum=(10*10+5)
+// console.log(sum)
+
+
+//16
+
+// sessionStorage.setItem("cool_strict",123)
+
+//17
+
+// const obj={
+//     a:"one",
+//     b:"two",
+//     a:"repeat"
+// }
+// console.log(obj)
+
+//18
+
+// for(let i=1;i<5;i++){
+//    if(i===3)continue;
+//    console.log(i)
+// }
+
+//19
+
+// const foo=()=>console.log("first")
+
+// const bar=()=>setTimeout(() => {
+//        console.log("second")
+// },);
+
+// const bazz=()=>console.log("third")
+
+// bar()
+// foo()
+// bazz()
